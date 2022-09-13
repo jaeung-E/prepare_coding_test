d0680f96665538262da1986e4843bdee268a9627
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2022-09-13 21:05:06 +0900

    chore: 풀이가 바뀌어있던 문제 수정

--- programmers/level_1/핸드폰_번호_가리기/jaeung.js
+++ programmers/level_1/핸드폰_번호_가리기/jaeung.js
@@ -1,12 +1,3 @@
-function solution(array, commands) {
-  const answer = [];
-
-  commands.forEach((command) => {
-    const [start, end, index] = command;
-    const newArray = array.slice(start - 1, end).sort((a, b) => a - b);
-
-    answer.push(newArray[index - 1]);
-  });
-
-  return answer;
+function solution(phone_number) {
+  return phone_number.slice(-4).padStart(phone_number.length, "*");
 }
