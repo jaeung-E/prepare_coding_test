bf94b192536a39e5e60561adc77297cb617e212f
minjongbaek <minjongbaek@gmail.com>
2022-09-23 15:36:06 +0900

    feat: 계단 오르기 풀이 추가

---
+++ boj/silver/계단_오르기/minjong.js
@@ -0,0 +1,17 @@
+const [N, ...stairs] = require('fs').readFileSync('../../input.txt', 'utf-8').trim().split('\n').map(Number);
+
+function solution(N, stairs) {
+  const dp = new Array(N).fill(0);
+
+  dp[0] = stairs[0];
+  dp[1] = stairs[0] + stairs[1];
+  dp[2] = Math.max(stairs[0], stairs[1]) + stairs[2];
+
+  for (let i = 3; i < N; i += 1) {
+    dp[i] = Math.max(dp[i - 3] + stairs[i - 1], dp[i - 2]) + stairs[i];
+  }
+
+  return dp[N - 1];
+}
+
+console.log(solution(N, stairs));
