1f2709227d6fc67a2e8eeb8ae5cb43e33adaefef
minjongbaek <minjongbaek@gmail.com>
2022-10-22 03:06:13 +0900

    refector: 나무 탈출 풀이 수정 리프 노드 검사 로직 변경

--- boj/silver/나무_탈출/minjong.js
+++ boj/silver/나무_탈출/minjong.js
@@ -4,44 +4,44 @@ const [N, ...edge] = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt', 'utf-8')
   .trim()
   .split('\n')
-  // .map(x => x.split(' ').map(Number));
+  .map(x => x.split(' ').map(Number));
 
 
 function solution(N, edge) {
   const tree = Array.from({ length: N + 1 }, () => []);
 
-  edge.forEach((x) => {
-    const [src, dst] = x.split(' ').map(Number);
+  edge.forEach(([src, dst]) => {
     tree[src].push(dst);
     tree[dst].push(src);
   });
 
   const visited = Array(N + 1).fill(false)
-  // const distance = Array(N + 1).fill(0);
 
   return dfs(1, tree, visited);
 }
 
-function dfs (src, tree, visited) {
-  const stack = [[src, 0]];
+function dfs(start, tree, visited) {
+  const stack = [[start, 0]];
+  visited[start] = true;
+  
   let leafDist = 0;
-  visited[src] = true;
 
   while (stack.length !== 0) {
     const [src, dist] = stack.pop();
 
+    if (src !== start && tree[src].length === 1) {
+      leafDist += dist;
+      continue;
+    }
+
     for (const dst of tree[src]) {
       if (!visited[dst]) {
         visited[dst] = true;
-        if (tree[dst].length === 1) leafDist += dist + 1;
         stack.push([dst, dist + 1]);
       }
     }
   }
-  // return (leafDist.reduce((acc, cur) => acc + cur) % 2 == 0 ? 'No' : 'Yes');
   return leafDist % 2 === 0 ? 'No' : 'Yes';
 }
-
-// console.log(+N, edge);
 
 console.log(solution(N, edge));
