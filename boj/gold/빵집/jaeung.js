f6b81e2200243948860896c6972969bbef570a8e
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2022-10-28 18:38:59 +0900

    refactor: 빵집 풀이 가독성 향상을 위한 변수명 및 코드 흐름 수정

--- boj/gold/빵집/jaeung.js
+++ boj/gold/빵집/jaeung.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync("dev/stdin").toString().trim().split("\r\n");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 const [R, C] = input.shift().split(" ").map(Number);
 const grids = input.map((str) => str.split(""));
 
@@ -18,31 +18,31 @@ function solution(R, C, grids) {
 
 function searchLine(x, visited, grids) {
   const stack = [[x, 0]];
-  const dx = [-1, 0, 1];
+  const dx = [1, 0, -1];
   const dy = [1, 1, 1];
 
   visited[x][0] = true;
 
-  loop: while (stack.length > 0) {
-    const [currentX, currentY] = stack[stack.length - 1];
+  while (stack.length > 0) {
+    const [currentX, currentY] = stack.pop();
     const isEnd = currentY === C - 1;
 
-    if (isEnd) return true;
+    if (isEnd) {
+      stack.forEach(([x, y]) => (visited[x][y] = false));
+      return true;
+    }
 
     for (let i = 0; i < dx.length; i++) {
       const nx = currentX + dx[i];
       const ny = currentY + dy[i];
-      const isVisit = isBound(nx, ny, R, C) && visited[nx][ny] === true;
-      const isEmpty = isBound(nx, ny, R, C) && grids[nx][ny] === ".";
+      const isVisit = isBound(nx, ny, R, C) && visited[nx][ny] === false;
+      const isBuilding = isBound(nx, ny, R, C) && grids[nx][ny] === "x";
 
-      if (!isVisit && isEmpty) {
+      if (isVisit && !isBuilding) {
         stack.push([nx, ny]);
         visited[nx][ny] = true;
-        continue loop;
       }
     }
-
-    stack.pop();
   }
 
   return false;
