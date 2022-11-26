afd5e8cfe707df5b885f844577db4b9800d801aa
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2022-11-27 02:30:53 +0900

    feat: 2048 (easy) 다른 방식의 풀이 추가

--- boj/gold/2048_(Easy)/jaeung.js
+++ boj/gold/2048_(Easy)/jaeung.js
@@ -1,141 +1,66 @@
 const fs = require("fs");
 const input = fs.readFileSync("dev/stdin").toString().trim().split("\r\n");
-const [[N], ...board] = input.map((str) => str.split(" ").map(Number));
+const [[N], ...initialBoard] = input.map((str) => str.split(" ").map(Number));
 
 console.log(solution());
 
 function solution() {
   let max = 0;
-  const stack = [[board, 0]];
-  const options = ["UP", "RIGHT", "DOWN", "LEFT"];
+  const stack = [[initialBoard, 0]];
 
   while (stack.length > 0) {
     const [board, count] = stack.pop();
+    let temp = copyArray(board);
 
     if (count === 5) {
       board.forEach((arr) => (max = Math.max(max, ...arr)));
       continue;
     }
 
-    options.forEach((option) => {
-      stack.push([moveBlock(option, board), count + 1]);
-    });
+    for (let i = 0; i < 4; i++) {
+      stack.push([moveBlock(temp), count + 1]);
+      temp = rotateBoard(temp);
+    }
   }
 
   return max;
 }
 
-function moveBlock(option, array) {
-  const newBoard = copyArray(array);
+function rotateBoard(board) {
+  const rotatedBoard = copyArray(board);
+
+  for (let x = 0; x < N; x++) {
+    for (let y = 0; y < N; y++) {
+      rotatedBoard[x][y] = board[y][N - x - 1];
+    }
+  }
+
+  return rotatedBoard;
+}
+
+function moveBlock(board) {
+  const newBoard = Array.from({ length: N }, () => Array(N).fill(0));
   const queue = [];
 
-  switch (option) {
-    case "UP":
-      for (let y = 0; y < N; y++) {
-        for (let x = 0; x < N; x++) {
-          if (newBoard[x][y] !== 0) {
-            queue.push(newBoard[x][y]);
-            newBoard[x][y] = 0;
-          }
-        }
+  for (let y = 0; y < N; y++) {
+    for (let x = 0; x < N; x++) {
+      if (board[x][y] !== 0) queue.push(board[x][y]);
+    }
 
-        let row = 0;
+    let row = 0;
 
-        while (queue.length > 0) {
-          const block = queue.shift();
+    while (queue.length > 0) {
+      const block = queue.shift();
 
-          if (newBoard[row][y] === 0) {
-            newBoard[row][y] = block;
-          } else if (newBoard[row][y] === block) {
-            newBoard[row][y] *= 2;
-            row++;
-          } else {
-            newBoard[++row][y] = block;
-          }
-        }
+      if (newBoard[row][y] === 0) {
+        newBoard[row][y] = block;
+      } else if (newBoard[row][y] === block) {
+        newBoard[row][y] *= 2;
+        row++;
+      } else {
+        newBoard[++row][y] = block;
       }
-
-      break;
-    case "RIGHT":
-      for (let x = 0; x < N; x++) {
-        for (let y = N - 1; y >= 0; y--) {
-          if (newBoard[x][y] !== 0) {
-            queue.push(newBoard[x][y]);
-            newBoard[x][y] = 0;
-          }
-        }
-
-        let col = N - 1;
-
-        while (queue.length > 0) {
-          const block = queue.shift();
-
-          if (newBoard[x][col] === 0) {
-            newBoard[x][col] = block;
-          } else if (newBoard[x][col] == block) {
-            newBoard[x][col] *= 2;
-            col--;
-          } else {
-            newBoard[x][--col] = block;
-          }
-        }
-      }
-
-      break;
-    case "DOWN":
-      for (let y = 0; y < N; y++) {
-        for (let x = N - 1; x >= 0; x--) {
-          if (newBoard[x][y] != 0) {
-            queue.push(newBoard[x][y]);
-            newBoard[x][y] = 0;
-          }
-        }
-
-        let row = N - 1;
-
-        while (queue.length > 0) {
-          const block = queue.shift();
-
-          if (newBoard[row][y] === 0) {
-            newBoard[row][y] = block;
-          } else if (newBoard[row][y] == block) {
-            newBoard[row][y] *= 2;
-            row--;
-          } else {
-            newBoard[--row][y] = block;
-          }
-        }
-      }
-
-      break;
-    case "LEFT":
-      for (let x = 0; x < N; x++) {
-        for (let y = 0; y < N; y++) {
-          if (newBoard[x][y] !== 0) {
-            queue.push(newBoard[x][y]);
-            newBoard[x][y] = 0;
-          }
-        }
-
-        let col = 0;
-
-        while (queue.length > 0) {
-          const block = queue.shift();
-
-          if (newBoard[x][col] === 0) {
-            newBoard[x][col] = block;
-          } else if (newBoard[x][col] == block) {
-            newBoard[x][col] *= 2;
-            col++;
-          } else {
-            newBoard[x][++col] = block;
-          }
-        }
-      }
-
-      break;
-    default:
-      console.error("유효하지 않은 옵션입니다.");
+    }
   }
 
   return newBoard;
